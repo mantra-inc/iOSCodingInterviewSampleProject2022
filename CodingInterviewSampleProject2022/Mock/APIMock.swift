b65b8b50cca4935786c50cc00465b6506d45bacd
vinter <ankasandora@gmail.com>
2022-07-07 15:25:09 +0900

    update card data

--- CodingInterviewSampleProject2022/Mock/APIMock.swift
+++ CodingInterviewSampleProject2022/Mock/APIMock.swift
@@ -6,7 +6,6 @@
 
 import Foundation
 
-// TODO: Move this to different module
 protocol API {
     /**
      This will return JSON object encoded by utf8
@@ -15,9 +14,10 @@ protocol API {
         - id: String : unique id for each card
         - nameEnglish: String : card name in English
         - nameJapanese: String : card name in Japanese
-        - textEnglish: String : card abilities in English
-        - textJapanese: String : card abilities in Japanese
-        - image_urls: List[String] : card image url
+        - abilityEnglish: String : card ability in English
+        - abilityJapanese: String : card ability in Japanese
+        - types: List[String]: the types of the card. One card will have more than one type but at lease it has one type.
+        - imageUrls: List[String] : card image urls. One card will have at lease one image url. You can just use the first url to preview the card
      */
     func getCards() async throws -> String
 }
@@ -50,9 +50,10 @@ extension APIImpl: API {
     func sleep(seconds: Int) async throws {
         try await Task.sleep(nanoseconds: 1 * 1_000_000_000)
     }
+    
+    // use below function to get all card data.
     func getCards() async throws -> String {
         try await sleep(seconds: 1)
         return cards
     }
-    
 }
